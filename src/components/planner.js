73b7e73e3813c05782d43467d60edfa34be8874f
GustavoAlex10 <gustavo_alex16@outlook.com>
2022-07-06 13:18:31 -0300

    planner

--- src/components/planner.js
+++ src/components/planner.js
@@ -1,28 +1,57 @@
 import React from "react";
 import styled from "styled-components";
 
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+
+const ContainerStatus = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 90px;
+`;
+
 const CardPlanner = styled.div`
-    width: 370px;
-    height: 500px;
+    width: 300px;
+    height: 250px;
     margin: 10px;
-    background-color: black;
+    background-color: #E2E6EE;
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
+    justify-content: center;
+    border-width: 0px;
     border-radius: 10px;
 `;
 
 const Day = styled.button`
-    width: 50px;
-    height: 40px;
-    margin: 10px;
-    background-color: white;    
+    width: 30px;
+    height: 30px;
+    margin-right: 5px;
+    margin-left: 5px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    background-color: #7C66F9;   
+    border-width: 0px; 
     border-radius: 10px;
+    &:hover,
+    &:focus {
+        background-color:orange;
+    }
+    &:active {
+        color: blue;
+      }
 `;
 
 const Text = styled.h1`
-    font-size: 20px;
-    color: gray;
+    font-size: 12px;
 `;
 
 const Notifications = styled.div`
@@ -32,178 +61,103 @@ const Notifications = styled.div`
 `;
 
 const Subscription = styled.div`
-    width: 370px;
-    height: 150px;
-    background-color: green;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width:300px;
+    height: 50px;
+    background-color: #E9E8F6;
     border-radius: 10px;
+    border-width: 0px;
     margin: 2px 10px;  
+    overflow: hidden;
+    padding: 5px;
+    &:hover,
+    &:focus {
+        background-color:#7C66F9;
+    }
+    &:active {
+        color: orange;
+      }
 `;
 
-const TextSubscription = styled.h1`
-    padding: 5px;  
-    font-size: 20px;
+const TextSubscription = styled.h1`  
+    font-size: 10px;
 `;
 
-const TextHolderSubs = styled.h1`
-    padding: 10px;  
-    font-size: 20px;
-    color: gray;
+const TextHolderSubs = styled.h1`  
+    font-size: 10px;
 `;
 
 const SubStatus = styled.img`
-    width: 100px;
-    height: 100px;
-    background-color: red;
-    border-radius: 10px;    
-    margin-left: 250px;
-    margin-top: -100px;
+    width: 40px;
+    height: 40px;
+    background-color: orange;
+    border-width: 0px;
+    border-radius: 10px;
+`;
+
+const AreaTextNotifications = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+    border-width: 0px;
 `;
 
 const Planner = () => {
 
+    const days = 32;
+    const daymap = []
+    const notifay = []
+
+    for (let i = 1; i < days; ++i) {
+        daymap.push(i)
+    }
+
+    for (let i = 1; i < days; ++i) {
+        notifay.push(i)
+    }
+
+
+
+
 
     return (
         <>
-            <CardPlanner>
-                <Day>
-                    <Text>1</Text>
-                </Day>
-                <Day>
-                    <Text>2</Text>
-                </Day>
-                <Day>
-                    <Text>3</Text>
-                </Day>
-                <Day>
-                    <Text>4</Text>
-                </Day>
-                <Day>
-                    <Text>5</Text>
-                </Day>
-                <Day>
-                    <Text>6</Text>
-                </Day>
-                <Day>
-                    <Text>7</Text>
-                </Day>
-                <Day>
-                    <Text>8</Text>
-                </Day>
-                <Day>
-                    <Text>9</Text>
-                </Day>
-                <Day>
-                    <Text>10</Text>
-                </Day>
-                <Day>
-                    <Text>11</Text>
-                </Day>
-                <Day>
-                    <Text>12</Text>
-                </Day>
-                <Day>
-                    <Text>13</Text>
-                </Day>
-                <Day>
-                    <Text>14</Text>
-                </Day>
-                <Day>
-                    <Text>15</Text>
-                </Day>
-                <Day>
-                    <Text>16</Text>
-                </Day>
-                <Day>
-                    <Text>17</Text>
-                </Day>
-                <Day>
-                    <Text>18</Text>
-                </Day>
-                <Day>
-                    <Text>19</Text>
-                </Day>
-                <Day>
-                    <Text>20</Text>
-                </Day>
-                <Day>
-                    <Text>21</Text>
-                </Day>
-                <Day>
-                    <Text>22</Text>
-                </Day>
-                <Day>
-                    <Text>23</Text>
-                </Day>
-                <Day>
-                    <Text>24</Text>
-                </Day>
-                <Day>
-                    <Text>25</Text>
-                </Day>
-                <Day>
-                    <Text>26</Text>
-                </Day>
-                <Day>
-                    <Text>27</Text>
-                </Day>
-                <Day>
-                    <Text>28</Text>
-                </Day>
-                <Day>
-                    <Text>29</Text>
-                </Day>
-                <Day>
-                    <Text>30</Text>
-                </Day>
-            </CardPlanner >
-            <Notifications>
-                <Subscription>
-                    <TextSubscription>
-                        Mark Hunter
-                    </TextSubscription>
-                    <TextHolderSubs>
-                        Mark Hunter
-                    </TextHolderSubs>
-                    <SubStatus />
-                    
-                </Subscription>
-                <Subscription>
-                    <TextSubscription>
-                        Mark Hunter
-                    </TextSubscription>
-                    <TextHolderSubs>
-                        Mark Hunter
-                    </TextHolderSubs>
-                    <SubStatus />                    
-                </Subscription>
-                <Subscription>
-                    <TextSubscription>
-                        Mark Hunter
-                    </TextSubscription>
-                    <TextHolderSubs>
-                        Mark Hunter
-                    </TextHolderSubs>
-                    <SubStatus />                    
-                </Subscription>
-                <Subscription>
-                    <TextSubscription>
-                        Mark Hunter
-                    </TextSubscription>
-                    <TextHolderSubs>
-                        Mark Hunter
-                    </TextHolderSubs>
-                    <SubStatus />                    
-                </Subscription>
-                <Subscription>
-                    <TextSubscription>
-                        Mark Hunter
-                    </TextSubscription>
-                    <TextHolderSubs>
-                        Mark Hunter
-                    </TextHolderSubs>
-                    <SubStatus />                    
-                </Subscription>
-            </Notifications>
+            <Container>
+                <CardPlanner>
+                    {
+                        daymap.map((a, i) => {
+                            return (
+                                <Day key={i}>
+                                    <Text>{a}</Text>
+                                </Day>
+                            )
+                        })
+                    }
+                </CardPlanner >
+                <Notifications>
+                    {
+                        notifay.map((a, i) => {
+                            return (
+                                <Subscription key={i}>
+                                    <AreaTextNotifications>
+                                        <TextSubscription>
+                                            Mark Hunter
+                                        </TextSubscription>
+                                        <TextHolderSubs>
+                                            Mark Hunter
+                                        </TextHolderSubs>
+                                    </AreaTextNotifications>
+                                    <ContainerStatus>
+                                        <SubStatus />
+                                    </ContainerStatus>
+                                </Subscription>
+                            )
+                        })
+                    }
+                </Notifications>
+            </Container >
         </>
     )
 }
